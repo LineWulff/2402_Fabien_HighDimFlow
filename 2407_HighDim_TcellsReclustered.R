e2e5e89e0493a9fb923ee1ace06a3daa939a9402
LineWulff <lw@himalaya.dk>
2024-07-16 10:58:10 -0600

    analysis updated to include density plots per cluster per marker and CD8 and CD4 specific analysis

--- 2407_HighDim_TcellsReclustered.R
+++ 2407_HighDim_TcellsReclustered.R
@@ -28,6 +28,8 @@ marker_cols <- panel$marker[!panel$marker %in% c("L-D","CD45","Tetramer")]
 # cl 6 are CD8 T cells
 
 AllTcells <- readRDS("24_07_15_AllTcellsreclustered_SPFvsrewilded_NONdownsampled.rds")
+#AllTcells <- readRDS("24_07_15_AllTcellsreclustered_SPFvsrewilded_NONdownsampled_analysed.rds")
+#AllTcells <- readRDS("24_07_16_AllTcellsreclustered_SPFvsrewilded_DOWNsampled_analysed.rds")
 CD4Tcells <- readRDS("24_07_15_CD4Tcellsreclustered_SPFvsrewilded_NONdownsampled.rds")
 CD8Tcells <- readRDS("24_07_15_CD8Tcellsreclustered_SPFvsrewilded_NONdownsampled.rds")
 
@@ -114,7 +116,7 @@ for (marker in marker_cols){
 }
 
 
-#### Now do clustering ####
+#' #### Now do clustering ####
 # Individual samples based on timestamps - "sampleID"
 # FlowSOM variables
 n_max <- 10 # set slightly higher than actual number as easier to combine some clusters after than having to few
@@ -141,10 +143,11 @@ for (i in seq(3,n_max)){
   dev.off()
 }
 
-## Plots of distributions
+#' #### Plots of distributions ####
 # run function from 2407_DistributionDF.R
 cl10_dist <- perc_df_calc(AllTcells, "sample", "cluster_10")
 cl10_dist$mice <- unlist(str_split(cl10_dist$sample,"_"))[seq(1,nrow(cl10_dist)*2,2)]
+cl10_dist$cluster <- factor(cl10_dist$cluster, levels = 1:10)
 # summarized table for errorbars
 cl10_dist_av <- data_summary(cl10_dist, varname="percentage", 
              groupnames=c("mice", "cluster"))
@@ -153,10 +156,12 @@ cl10_dist_av$mice <- factor(cl10_dist_av$mice, levels = c("SPF","re-wilded"))
 
 ## Plots
 ## stacked
-ggplot(cl10_dist, aes(x=sample, y=percentage, fill=cluster))+
+ggplot(cl10_dist, aes(x=sample, y=percentage, fill=cluster))+ 
   geom_bar(position = "stack", stat="identity", colour="black")+
   theme_classic()+
+  ylab("% of total T cells per sample")+
   theme(axis.text.x = element_text(angle=90))
+ggsave(paste(RAID_dir,"/output/",dato,"AllTcellsreclustered_cl10distribution_stacked.pdf",sep=""), height = 4, width = 4)
 
 ## side by side
 ggplot(cl10_dist_av, aes(x=mice, y=percentage, fill=cluster))+
@@ -165,13 +170,337 @@ ggplot(cl10_dist_av, aes(x=mice, y=percentage, fill=cluster))+
                 position=position_dodge(.9))+
   facet_grid(.~cluster)+
   theme_classic()+
-  xlab("")+
+  xlab("")+ylab("% of total T cells per sample")+
   theme(axis.text.x = element_text(angle = 90))
+ggsave(paste(RAID_dir,"/output/",dato,"AllTcellsreclustered_cl10distribution_sidebyside.pdf",sep=""), height = 4, width = 5)
 
+#' #### Plots of markers - density per cluster ####
+for (marker in marker_cols){
+  plot_mark <- ggplot(data = AllTcells, aes(x = AllTcells[,marker], group = cluster_10, fill = cluster_10)) +
+    geom_density(adjust = 1.5)+
+    facet_wrap(~cluster_10)+
+    theme_classic()+
+    xlab(marker)+
+    theme(axis.text.y = element_blank(), axis.ticks = element_blank())
+  if (str_detect(marker,"/")){marker <- str_replace(marker,"/","-")}
+  pdf(paste(RAID_dir,"/output/",dato,"AllTcellsreclustered_cluster10_density_",marker,"_plot.pdf", sep=""), height = 6, width = 6)
+  print(plot_mark)
+  dev.off()
+}
 
-#
+#' #### save objects as analysed for later analysis ####
 saveRDS(AllTcells, file = paste(dato,"AllTcellsreclustered_SPFvsrewilded_DOWNsampled_analysed.rds",sep = "_"))
 saveRDS(AllTcells, file = paste(dato,"AllTcellsreclustered_SPFvsrewilded_NONdownsampled_analysed.rds",sep = "_"))
 
 
 
+
+#### --- CD4 T cells ---- ####
+#' #### ---- Running UMAP ---- ####
+# possibility of downsampling
+# downsample to equal amount of cells in each group/sample/what makes sense
+# check group with fewest observations
+table(CD4Tcells$sample)
+
+# downsampled for overall analysis of CD45+
+CD4Tcells <- downsample(CD4Tcells, cat_col = "sample")
+# or non downsampled for T cell analysis
+# just don't run above
+
+
+# prepare data for umapr (matrix format required)
+data_umap <- CD4Tcells[, marker_cols]
+data_umap <- as.matrix(data_umap)
+dups <- duplicated(data_umap)
+data_umap <- data_umap[!dups, ]
+
+# run umap, takes a long time with many cells, downsample first to test
+umap_emb <- umap(data_umap, scale = "maxabs") 
+
+# prepare umap embedding output data for plot
+CD4Tcells_umap <- as.data.frame(umap_emb)
+colnames(CD4Tcells_umap) <- c("UMAP_1", "UMAP_2")
+head(CD4Tcells_umap);dim(CD4Tcells_umap)
+CD4Tcells[,c("UMAP_1","UMAP_2")] <- CD4Tcells_umap
+
+
+#### Plot each individual group as seperate contour on top of total cells
+ggplot(CD4Tcells, aes(x = UMAP_1, y = UMAP_2, colour = mice))+ 
+  geom_point_rast(size=0.1)+
+  theme_classic()
+
+ggplot(CD4Tcells, aes(x = UMAP_1, y = UMAP_2, colour=mice))+ 
+  #geom_point_rast(size=0.1, colour="lightgrey")+
+  geom_density_2d()+
+  xlim(c(-13,13))+ylim(c(-15,17))+
+  theme_classic()
+ggsave(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_UMAP_Contour_rewildvsSPF_plot.pdf",sep=""), height = 4, width = 4)
+
+# rewilded only
+ggplot(CD4Tcells, aes(x = UMAP_1, y = UMAP_2))+ 
+  geom_point_rast(colour="lightgrey", size=1)+
+  geom_density_2d(data=CD4Tcells[CD4Tcells$mice=="re-wilded",],
+                  aes(x = UMAP_1, y = UMAP_2),
+                  colour="#F8766D")+
+  theme_classic()+
+  xlim(c(-13,13))+ylim(c(-15,17))+
+  theme(axis.text = element_blank(), axis.ticks = element_blank())
+ggsave(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_UMAP_rewildedContour_plot.pdf",sep=""), height = 4, width = 4)
+
+#SPF only
+ggplot(CD4Tcells, aes(x = UMAP_1, y = UMAP_2))+ 
+  geom_point_rast(colour="lightgrey", size=1)+
+  geom_density_2d(data=CD4Tcells[CD4Tcells$mice=="SPF",],
+                  aes(x = UMAP_1, y = UMAP_2),
+                  colour="#00BFC4")+
+  theme_classic()+
+  xlim(c(-13,13))+ylim(c(-15,17))+
+  theme(axis.text = element_blank(), axis.ticks = element_blank())
+ggsave(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_UMAP_SPFContour_plot.pdf",sep=""), height = 4, width = 4)
+
+
+#### Colour by individual marker and save pdf versions
+for (marker in marker_cols){
+  plot_mark <- ggplot(CD4Tcells, aes(x = UMAP_1, y = UMAP_2, colour=data_umap[,marker]))+ 
+    geom_point_rast(size=0.1)+
+    scale_colour_viridis_c(option = "plasma")+
+    theme_classic()+
+    labs(colour=marker)+
+    theme(axis.text = element_blank(), axis.ticks = element_blank())
+  if (str_detect(marker,"/")){marker <- str_replace(marker,"/","-")}
+  pdf(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_UMAP_",marker,"_plot.pdf", sep=""), height = 4, width = 4)
+  print(plot_mark)
+  dev.off()
+}
+
+
+#' #### Now do clustering ####
+# Individual samples based on timestamps - "sampleID"
+# FlowSOM variables
+n_max <- 10 # set slightly higher than actual number as easier to combine some clusters after than having to few
+seed <- 13
+
+for (i in seq(3,n_max)){
+  n_meta <- i
+  # Compute the FlowSOM object
+  fsom <- FlowSOM(input = as.matrix(data_umap),
+                  scale = FALSE,
+                  colsToUse = marker_cols,
+                  seed = seed,
+                  nClus = n_meta)
+  # add to FC file
+  CD4Tcells[paste("cluster", i, sep = "_")] <- GetMetaclusters(fsom)
+  # plot and check matches
+  clus_plot <- ggplot(CD4Tcells, aes(x=UMAP_1,y=UMAP_2,colour = CD4Tcells[,paste("cluster", i, sep = "_")]))+
+    geom_point()+
+    labs(colour=paste("cluster", i, sep = "_"))+
+    theme_classic()+
+    theme(axis.text = element_blank(), axis.ticks = element_blank())
+  pdf(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_UMAP_","cluster", i,"_plot.pdf", sep=""), height = 4, width = 4)
+  print(clus_plot)
+  dev.off()
+}
+
+#' #### Plots of distributions ####
+# run function from 2407_DistributionDF.R
+cl7_dist <- perc_df_calc(CD4Tcells, "sample", "cluster_7")
+cl7_dist$mice <- unlist(str_split(cl7_dist$sample,"_"))[seq(1,nrow(cl7_dist)*2,2)]
+cl7_dist$cluster <- factor(cl7_dist$cluster, levels = 1:10)
+# summarized table for errorbars
+cl7_dist_av <- data_summary(cl7_dist, varname="percentage", 
+                             groupnames=c("mice", "cluster"))
+cl7_dist_av$cluster <- factor(cl7_dist_av$cluster, levels = 1:10)
+cl7_dist_av$mice <- factor(cl7_dist_av$mice, levels = c("SPF","re-wilded"))
+
+## Plots
+## stacked
+ggplot(cl7_dist, aes(x=sample, y=percentage, fill=cluster))+ 
+  geom_bar(position = "stack", stat="identity", colour="black")+
+  theme_classic()+
+  ylab("% of total T cells per sample")+
+  theme(axis.text.x = element_text(angle=90))
+ggsave(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_cl7distribution_stacked.pdf",sep=""), height = 4, width = 4)
+
+## side by side
+ggplot(cl7_dist_av, aes(x=mice, y=percentage, fill=cluster))+
+  geom_bar(position = "dodge", stat = "identity", colour = "black")+
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=.2,
+                position=position_dodge(.9))+
+  facet_grid(.~cluster)+
+  theme_classic()+
+  xlab("")+ylab("% of total T cells per sample")+
+  theme(axis.text.x = element_text(angle = 90))
+ggsave(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_cl7distribution_sidebyside.pdf",sep=""), height = 4, width = 5)
+
+#' #### Plots of markers - density per cluster ####
+for (marker in marker_cols){
+  plot_mark <- ggplot(data = CD4Tcells, aes(x = CD4Tcells[,marker], group = cluster_7, fill = cluster_10)) +
+    geom_density(adjust = 1.5)+
+    facet_wrap(~cluster_10)+
+    theme_classic()+
+    xlab(marker)+
+    theme(axis.text.y = element_blank(), axis.ticks = element_blank())
+  if (str_detect(marker,"/")){marker <- str_replace(marker,"/","-")}
+  pdf(paste(RAID_dir,"/output/",dato,"CD4Tcellsreclustered_cluster7_density_",marker,"_plot.pdf", sep=""), height = 6, width = 6)
+  print(plot_mark)
+  dev.off()
+}
+
+#' #### save objects as analysed for later analysis ####
+saveRDS(CD4Tcells, file = paste(dato,"CD4Tcellsreclustered_SPFvsrewilded_DOWNsampled_analysed.rds",sep = "_"))
+saveRDS(CD4Tcells, file = paste(dato,"CD4Tcellsreclustered_SPFvsrewilded_NONdownsampled_analysed.rds",sep = "_"))
+
+#### --- CD8 T cells --- ####
+#' #### ---- Running UMAP ---- ####
+# possibility of downsampling
+# downsample to equal amount of cells in each group/sample/what makes sense
+# check group with fewest observations
+table(CD8Tcells$sample)
+
+# downsampled for overall analysis of CD45+
+CD8Tcells <- downsample(CD8Tcells, cat_col = "sample")
+# or non downsampled for T cell analysis
+# just don't run above
+
+
+# prepare data for umapr (matrix format required)
+data_umap <- CD8Tcells[, marker_cols]
+data_umap <- as.matrix(data_umap)
+dups <- duplicated(data_umap)
+data_umap <- data_umap[!dups, ]
+
+# run umap, takes a long time with many cells, downsample first to test
+umap_emb <- umap(data_umap, scale = "maxabs") 
+
+# prepare umap embedding output data for plot
+CD8Tcells_umap <- as.data.frame(umap_emb)
+colnames(CD8Tcells_umap) <- c("UMAP_1", "UMAP_2")
+head(CD8Tcells_umap);dim(CD8Tcells_umap)
+CD8Tcells[,c("UMAP_1","UMAP_2")] <- CD8Tcells_umap
+
+
+#### Plot each individual group as seperate contour on top of total cells
+ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2, colour = mice))+ 
+  geom_point_rast(size=0.1)+
+  theme_classic()
+
+ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2, colour=mice))+ 
+  #geom_point_rast(size=0.1, colour="lightgrey")+
+  geom_density_2d()+
+  xlim(c(-13,13))+ylim(c(-15,17))+
+  theme_classic()
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_Contour_rewildvsSPF_plot.pdf",sep=""), height = 4, width = 4)
+
+# rewilded only
+ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2))+ 
+  geom_point_rast(colour="lightgrey", size=1)+
+  geom_density_2d(data=CD8Tcells[CD8Tcells$mice=="re-wilded",],
+                  aes(x = UMAP_1, y = UMAP_2),
+                  colour="#F8766D")+
+  theme_classic()+
+  xlim(c(-13,13))+ylim(c(-15,17))+
+  theme(axis.text = element_blank(), axis.ticks = element_blank())
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_rewildedContour_plot.pdf",sep=""), height = 4, width = 4)
+
+#SPF only
+ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2))+ 
+  geom_point_rast(colour="lightgrey", size=1)+
+  geom_density_2d(data=CD8Tcells[CD8Tcells$mice=="SPF",],
+                  aes(x = UMAP_1, y = UMAP_2),
+                  colour="#00BFC4")+
+  theme_classic()+
+  xlim(c(-13,13))+ylim(c(-15,17))+
+  theme(axis.text = element_blank(), axis.ticks = element_blank())
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_SPFContour_plot.pdf",sep=""), height = 4, width = 4)
+
+
+#### Colour by individual marker and save pdf versions
+for (marker in marker_cols){
+  plot_mark <- ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2, colour=data_umap[,marker]))+ 
+    geom_point_rast(size=0.1)+
+    scale_colour_viridis_c(option = "plasma")+
+    theme_classic()+
+    labs(colour=marker)+
+    theme(axis.text = element_blank(), axis.ticks = element_blank())
+  if (str_detect(marker,"/")){marker <- str_replace(marker,"/","-")}
+  pdf(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_",marker,"_plot.pdf", sep=""), height = 4, width = 4)
+  print(plot_mark)
+  dev.off()
+}
+
+
+#' #### Now do clustering ####
+# Individual samples based on timestamps - "sampleID"
+# FlowSOM variables
+n_max <- 10 # set slightly higher than actual number as easier to combine some clusters after than having to few
+seed <- 13
+
+for (i in seq(3,n_max)){
+  n_meta <- i
+  # Compute the FlowSOM object
+  fsom <- FlowSOM(input = as.matrix(data_umap),
+                  scale = FALSE,
+                  colsToUse = marker_cols,
+                  seed = seed,
+                  nClus = n_meta)
+  # add to FC file
+  CD8Tcells[paste("cluster", i, sep = "_")] <- GetMetaclusters(fsom)
+  # plot and check matches
+  clus_plot <- ggplot(CD8Tcells, aes(x=UMAP_1,y=UMAP_2,colour = CD8Tcells[,paste("cluster", i, sep = "_")]))+
+    geom_point()+
+    labs(colour=paste("cluster", i, sep = "_"))+
+    theme_classic()+
+    theme(axis.text = element_blank(), axis.ticks = element_blank())
+  pdf(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_","cluster", i,"_plot.pdf", sep=""), height = 4, width = 4)
+  print(clus_plot)
+  dev.off()
+}
+
+#' #### Plots of distributions ####
+# run function from 2407_DistributionDF.R
+cl7_dist <- perc_df_calc(CD8Tcells, "sample", "cluster_7")
+cl7_dist$mice <- unlist(str_split(cl7_dist$sample,"_"))[seq(1,nrow(cl7_dist)*2,2)]
+cl7_dist$cluster <- factor(cl7_dist$cluster, levels = 1:10)
+# summarized table for errorbars
+cl7_dist_av <- data_summary(cl7_dist, varname="percentage", 
+                             groupnames=c("mice", "cluster"))
+cl7_dist_av$cluster <- factor(cl7_dist_av$cluster, levels = 1:10)
+cl7_dist_av$mice <- factor(cl7_dist_av$mice, levels = c("SPF","re-wilded"))
+
+## Plots
+## stacked
+ggplot(cl7_dist, aes(x=sample, y=percentage, fill=cluster))+ 
+  geom_bar(position = "stack", stat="identity", colour="black")+
+  theme_classic()+
+  ylab("% of total T cells per sample")+
+  theme(axis.text.x = element_text(angle=90))
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cl7distribution_stacked.pdf",sep=""), height = 4, width = 4)
+
+## side by side
+ggplot(cl7_dist_av, aes(x=mice, y=percentage, fill=cluster))+
+  geom_bar(position = "dodge", stat = "identity", colour = "black")+
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=.2,
+                position=position_dodge(.9))+
+  facet_grid(.~cluster)+
+  theme_classic()+
+  xlab("")+ylab("% of total T cells per sample")+
+  theme(axis.text.x = element_text(angle = 90))
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cl7distribution_sidebyside.pdf",sep=""), height = 4, width = 5)
+
+#' #### Plots of markers - density per cluster ####
+for (marker in marker_cols){
+  plot_mark <- ggplot(data = CD8Tcells, aes(x = CD8Tcells[,marker], group = cluster_7, fill = cluster_10)) +
+    geom_density(adjust = 1.5)+
+    facet_wrap(~cluster_10)+
+    theme_classic()+
+    xlab(marker)+
+    theme(axis.text.y = element_blank(), axis.ticks = element_blank())
+  if (str_detect(marker,"/")){marker <- str_replace(marker,"/","-")}
+  pdf(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cluster7_density_",marker,"_plot.pdf", sep=""), height = 6, width = 6)
+  print(plot_mark)
+  dev.off()
+}
+
+#' #### save objects as analysed for later analysis ####
+saveRDS(CD8Tcells, file = paste(dato,"CD8Tcellsreclustered_SPFvsrewilded_DOWNsampled_analysed.rds",sep = "_"))
+saveRDS(CD8Tcells, file = paste(dato,"CD8Tcellsreclustered_SPFvsrewilded_NONdownsampled_analysed.rds",sep = "_"))
