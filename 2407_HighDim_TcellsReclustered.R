b7035d25bc24b03a05daf584ab210b20ec0b2c57
LineWulff <lw@himalaya.dk>
2024-07-16 14:51:22 -0600

    extra cl dist for CD8 T cells

--- 2407_HighDim_TcellsReclustered.R
+++ 2407_HighDim_TcellsReclustered.R
@@ -363,6 +363,7 @@ table(CD8Tcells$sample)
 
 # downsampled for overall analysis of CD45+
 CD8Tcells <- downsample(CD8Tcells, cat_col = "sample")
+CD8Tcells$mice <- factor(CD8Tcells$mice, levels = c("SPF","re-wilded"))
 # or non downsampled for T cell analysis
 # just don't run above
 
@@ -391,18 +392,39 @@ ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2, colour = mice))+
 ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2, colour=mice))+ 
   #geom_point_rast(size=0.1, colour="lightgrey")+
   geom_density_2d()+
-  xlim(c(-13,13))+ylim(c(-15,17))+
+  xlim(c(-8,8))+ylim(c(-8,8))+
   theme_classic()
 ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_Contour_rewildvsSPF_plot.pdf",sep=""), height = 4, width = 4)
+
+ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2))+
+  geom_point(colour="darkgrey")+
+  stat_density_2d(geom = "polygon", contour = TRUE,
+                  aes(fill = after_stat(level)), alpha = 0.5, colour = "black",
+                  bins = 5)+
+  facet_grid(.~mice)+
+  xlim(c(-8,8))+ylim(c(-8,8))+
+  scale_fill_distiller(palette = "Blues", direction = 1) +
+  theme_classic()
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_Contour_rewildvsSPF_plot2blue.pdf",sep=""), height = 4, width = 8)
+
+
+ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2, colour=mice))+
+  geom_point(colour="lightgrey",alpha=0.3)+
+  stat_density_2d(geom = "polygon", aes(alpha = ..level.., fill = mice), bins=5)+
+  facet_grid(.~mice)+
+  xlim(c(-8,8))+ylim(c(-8,8))+
+  theme_classic()
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_Contour_rewildvsSPF_plot3sepcols.pdf",sep=""), height = 4, width = 8)
+
 
 # rewilded only
 ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2))+ 
   geom_point_rast(colour="lightgrey", size=1)+
   geom_density_2d(data=CD8Tcells[CD8Tcells$mice=="re-wilded",],
                   aes(x = UMAP_1, y = UMAP_2),
-                  colour="#F8766D")+
+                  colour="#00BFC4")+
   theme_classic()+
-  xlim(c(-13,13))+ylim(c(-15,17))+
+  xlim(c(-8,8))+ylim(c(-8,8))+
   theme(axis.text = element_blank(), axis.ticks = element_blank())
 ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_rewildedContour_plot.pdf",sep=""), height = 4, width = 4)
 
@@ -411,9 +433,9 @@ ggplot(CD8Tcells, aes(x = UMAP_1, y = UMAP_2))+
   geom_point_rast(colour="lightgrey", size=1)+
   geom_density_2d(data=CD8Tcells[CD8Tcells$mice=="SPF",],
                   aes(x = UMAP_1, y = UMAP_2),
-                  colour="#00BFC4")+
+                  colour="#F8766D")+
   theme_classic()+
-  xlim(c(-13,13))+ylim(c(-15,17))+
+  xlim(c(-8,8))+ylim(c(-8,8))+
   theme(axis.text = element_blank(), axis.ticks = element_blank())
 ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_UMAP_SPFContour_plot.pdf",sep=""), height = 4, width = 4)
 
@@ -462,26 +484,26 @@ for (i in seq(3,n_max)){
 
 #' #### Plots of distributions ####
 # run function from 2407_DistributionDF.R
-cl7_dist <- perc_df_calc(CD8Tcells, "sample", "cluster_7")
-cl7_dist$mice <- unlist(str_split(cl7_dist$sample,"_"))[seq(1,nrow(cl7_dist)*2,2)]
-cl7_dist$cluster <- factor(cl7_dist$cluster, levels = 1:10)
+cl10_dist <- perc_df_calc(CD8Tcells, "sample", "cluster_10")
+cl10_dist$mice <- unlist(str_split(cl10_dist$sample,"_"))[seq(1,nrow(cl10_dist)*2,2)]
+cl10_dist$cluster <- factor(cl10_dist$cluster, levels = 1:10)
 # summarized table for errorbars
-cl7_dist_av <- data_summary(cl7_dist, varname="percentage", 
+cl10_dist_av <- data_summary(cl10_dist, varname="percentage", 
                              groupnames=c("mice", "cluster"))
-cl7_dist_av$cluster <- factor(cl7_dist_av$cluster, levels = 1:10)
-cl7_dist_av$mice <- factor(cl7_dist_av$mice, levels = c("SPF","re-wilded"))
+cl10_dist_av$cluster <- factor(cl10_dist_av$cluster, levels = 1:10)
+cl10_dist_av$mice <- factor(cl10_dist_av$mice, levels = c("SPF","re-wilded"))
 
 ## Plots
 ## stacked
-ggplot(cl7_dist, aes(x=sample, y=percentage, fill=cluster))+ 
+ggplot(cl10_dist, aes(x=sample, y=percentage, fill=cluster))+ 
   geom_bar(position = "stack", stat="identity", colour="black")+
   theme_classic()+
   ylab("% of total T cells per sample")+
   theme(axis.text.x = element_text(angle=90))
-ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cl7distribution_stacked.pdf",sep=""), height = 4, width = 4)
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cl10distribution_stacked.pdf",sep=""), height = 4, width = 4)
 
 ## side by side
-ggplot(cl7_dist_av, aes(x=mice, y=percentage, fill=cluster))+
+ggplot(cl10_dist_av, aes(x=mice, y=percentage, fill=cluster))+
   geom_bar(position = "dodge", stat = "identity", colour = "black")+
   geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=.2,
                 position=position_dodge(.9))+
@@ -489,18 +511,18 @@ ggplot(cl7_dist_av, aes(x=mice, y=percentage, fill=cluster))+
   theme_classic()+
   xlab("")+ylab("% of total T cells per sample")+
   theme(axis.text.x = element_text(angle = 90))
-ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cl7distribution_sidebyside.pdf",sep=""), height = 4, width = 5)
+ggsave(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cl10distribution_sidebyside.pdf",sep=""), height = 4, width = 5)
 
 #' #### Plots of markers - density per cluster ####
 for (marker in marker_cols){
-  plot_mark <- ggplot(data = CD8Tcells, aes(x = CD8Tcells[,marker], group = cluster_7, fill = cluster_10)) +
+  plot_mark <- ggplot(data = CD8Tcells, aes(x = CD8Tcells[,marker], group = cluster_10, fill = cluster_10)) +
     geom_density(adjust = 1.5)+
     facet_wrap(~cluster_10)+
     theme_classic()+
     xlab(marker)+
     theme(axis.text.y = element_blank(), axis.ticks = element_blank())
   if (str_detect(marker,"/")){marker <- str_replace(marker,"/","-")}
-  pdf(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cluster7_density_",marker,"_plot.pdf", sep=""), height = 6, width = 6)
+  pdf(paste(RAID_dir,"/output/",dato,"CD8Tcellsreclustered_cluster10_density_",marker,"_plot.pdf", sep=""), height = 6, width = 6)
   print(plot_mark)
   dev.off()
 }
